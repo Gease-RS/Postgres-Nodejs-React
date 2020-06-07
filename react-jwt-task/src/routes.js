edbb0a6233665a9e267d9e359bd0fc8fd57777de
Gease-RS <geasebrasil@gmail.com>
2020-06-06 22:10:58 -0300

    Navbar

--- react-jwt-task/src/routes.js
+++ react-jwt-task/src/routes.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TaskList from './pages/TaskList'
+import TaskForm from './pages/TaskForm'
+import Navbar from './Navbar'
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/task-list" component={TaskList} />
-            </Switch>
+            <div className="container">
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/task-list" component={TaskList} />
+                    <Route exact path="/task-form" component={TaskForm} />
+                </Switch>
+            </div>
         </BrowserRouter>
     )
 }
